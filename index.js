442a95671c7fa7aee4a1d53b65fe71666e0c2025
Jason Stillwell <jstillwell@wheels.co>
2021-02-21 20:50:51 -0800

    simple one song verison

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const Discord = require('discord.js');
 const {
   prefix,
   token,
+  channel_id,
+  guild_id,
+  filename
 } = require('./config.json');
 const ytdl = require('ytdl-core');
 
@@ -9,9 +12,14 @@ const ytdl = require('ytdl-core');
 const client = new Discord.Client();
 client.login(token);
 
+var guild;
 
 client.once('ready', () => {
   console.log('Ready!');
+
+  startSong();
+
+
 });
 client.once('reconnecting', () => {
   console.log('Reconnecting!');
@@ -20,10 +28,31 @@ client.once('disconnect', () => {
   console.log('Disconnect!');
 });
 
+async function startSong() {
+
+  var connection = await (client.channels.cache.get(channel_id).join());
+
+  playSong(connection, filename)
+
+}
+
+async function playSong(connection, filename) {
+
+  const dispatcher = connection
+    .play(filename)
+    .on("finish", () => {
+      playSong(connection, filename);
+    })
+    .on("error", error => console.error(error));
+  // dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  // serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+
+}
+
 function play(guild, song) {
   const serverQueue = queue.get(guild.id);
   if (!song) {
-    serverQueue.voiceChannel.leave();
+    // serverQueue.voiceChannel.leave();
     queue.delete(guild.id);
     return;
   }
@@ -65,35 +94,35 @@ async function execute(message, serverQueue) {
 
   if (!serverQueue) {
 
-    // Creating the contract for our queue
-    const queueContruct = {
-      textChannel: message.channel,
-      voiceChannel: voiceChannel,
-      connection: null,
-      songs: [],
-      volume: 5,
-      playing: true,
-    };
-    // Setting the queue using our contract
-    queue.set(message.guild.id, queueContruct);
-    // Pushing the song to our songs array
-    queueContruct.songs.push(song);
+  // Creating the contract for our queue
+  const queueContruct = {
+    textChannel: message.channel,
+    voiceChannel: voiceChannel,
+    connection: null,
+    songs: [],
+    volume: 5,
+    playing: true,
+  };
+  // Setting the queue using our contract
+  queue.set(message.guild.id, queueContruct);
+  // Pushing the song to our songs array
+  queueContruct.songs.push(song);
 
-    try {
-      // Here we try to join the voicechat and save our connection into our object.
-      var connection = await voiceChannel.join();
-      queueContruct.connection = connection;
-      // Calling the play function to start a song
-      play(message.guild, queueContruct.songs[0]);
-    } catch (err) {
-      // Printing the error message if the bot fails to join the voicechat
-      console.log(err);
-      queue.delete(message.guild.id);
-      return message.channel.send(err);
-    }
+  try {
+    // Here we try to join the voicechat and save our connection into our object.
+    var connection = await voiceChannel.join();
+    queueContruct.connection = connection;
+    // Calling the play function to start a song
+    play(message.guild, queueContruct.songs[0]);
+  } catch (err) {
+    // Printing the error message if the bot fails to join the voicechat
+    console.log(err);
+    queue.delete(message.guild.id);
+    return message.channel.send(err);
+  }
 
   } else {
-    serverQueue.songs.push(song);
+      serverQueue.songs.push(song);
     console.log(serverQueue.songs);
     return message.channel.send(`${song.title} has been added to the queue!`);
   }
